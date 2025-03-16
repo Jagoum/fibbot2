37b211d78dd819084726ed322704145ed814e7a3
jagoum <jagoum24@gmail.com>
2025-03-16 04:20:30 +0100

    chore{modified} added some codes to handle error cases like writing to the file

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ use std::fs::write;
 use std::process::exit;
 
 fn main() {
-    let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
+    let github_output_path = match env::var("GITHUB_OUTPUT") {
+        Ok(output) => {println!("File created successfully"); output},
+        Err(e) => {eprintln!("Error: {e}"); return;},
+    };
 
     let args: Vec<String> = env::args().collect();
     let error = &args[1];
 
     if !error.is_empty() {
         eprintln!("Error: {error}");
-        write(github_output_path, format!("error={error}")).unwrap();
+        match write(github_output_path, format!("error={error}\n")) {
+            Ok(_) => {println!("Wrote to file")},
+            Err(_) => {eprintln!("Could write to file")},
+        };
         exit(1);
     }
 }
